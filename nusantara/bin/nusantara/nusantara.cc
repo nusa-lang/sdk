86d4653219f1b29195bbbbec10122ae57332120c
Fern-Aerell <fernaerell2020@gmail.com>
2025-03-05 23:50:01 +0700

    update: Implementation lexer in Nusantara binary

--- nusantara/bin/nusantara/nusantara.cc
+++ nusantara/bin/nusantara/nusantara.cc
@@ -1,7 +1,33 @@
+#include "nusantara/lexer/lexer.h"
+#include "nusantara/lexer/token/token.h"
+#include "nusantara/lexer/token/token_type.h"
+#include <exception>
 #include <llvm/Support/raw_ostream.h>
 
-int main()
+int main(int argc, char* argv[])
 {
-    llvm::outs() << "Hello World!\n";
-    return 0;
+    try
+    {
+        if (argc == 1)
+            return 0;
+
+        auto lexer{nusantara::Lexer::file(argv[1])};
+
+        nusantara::Token token{lexer.nextToken()};
+        while (token.type != nusantara::TokenType::NEOF)
+        {
+            llvm::outs() << token << "\n";
+            token = lexer.nextToken();
+        }
+
+        llvm::outs() << token << "\n";
+
+        return 0;
+    }
+    catch (const std::exception& error)
+    {
+        llvm::outs() << error.what() << "\n";
+    }
+
+    return 1;
 }
