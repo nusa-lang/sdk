935e4bf1b38df43d6ceae5cebf5e875f511d12fa
Fern-Aerell <fernaerell2020@gmail.com>
2025-03-19 10:49:04 +0700

    [nusantara][TokenType]: Tidy up the type order

--- nusantara/include/nusantara/lexer/token/token_type.h
+++ nusantara/include/nusantara/lexer/token/token_type.h
@@ -38,11 +38,13 @@ enum class TokenType
     KW_USE, // pattern: 'use'
 
     // Literals
-    LIT_STR, // pattern: ('\'' ~'\''* '\'' | '"' ~'"'* '"')
     LIT_NUM, // pattern: [0-9]+ ('.' [0-9]+)?
+    LIT_STR, // pattern: ('\'' ~'\''* '\'' | '"' ~'"'* '"')
+
+    // Other
+    ID, // pattern: [a-zA-Z_][a-zA-Z0-9_]*
 
     // System
-    ID,      // pattern: [a-zA-Z_][a-zA-Z0-9_]*
     UNKNOWN, // unknown
     NEOF,    // eof
 };
